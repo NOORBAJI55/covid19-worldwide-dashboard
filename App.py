f86a97e976a7341919baafdd72ed8c88f1c29568
Shaik Noor Baji <137255603+NOORBAJI55@users.noreply.github.com>
2025-08-28 20:38:49 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,13 +5,9 @@ import plotly.express as px
 import datetime 
 
 # Load dataset
-# df = pd.read_csv("https://catalog.ourworldindata.org/garden/covid/latest/compact/compact.csv")
+df = pd.read_csv("https://catalog.ourworldindata.org/garden/covid/latest/compact/compact.csv")
 
 # df = pd.read_csv("covid_worldwide_compact.csv")  # use this if u download the dataset
-
-df = pd.read_csv("covid_worldwide_compact.csv", sep=",", engine="python")
-st.write(df.columns.tolist())
-
 
 # Drop rows with no country info
 df = df.dropna(subset=["country"])
